e712c81f6bb102a78363c6c05bca27a7527c1638
Sophie Walton <sophiewalton@Sophies-MacBook-Pro.local>
2021-01-13 10:53:23 -0800

    updated recoding for doublets and subsections - py files

--- rnai_scripts.py
+++ rnai_scripts.py
@@ -6,6 +6,7 @@ import Bio
 import scipy.spatial
 import itertools as it 
 
+
 # create dictionaries of amino acids and codons 
 gencode = {
     'ATA':'I', 'ATC':'I', 'ATT':'I', 'ATG':'M',
@@ -48,6 +49,23 @@ aminoacidcode = {'I': ['ATA', 'ATC', 'ATT'],
  '*': ['TAA', 'TAG', 'TGA'],
  'C': ['TGC', 'TGT'],
  'W': ['TGG']}
+
+
+def get_opposite_codon_frequencies(codon_frequencies_dic):
+    opposite_codon_frequencies ={}
+
+    
+    for key in codon_frequencies_dic.keys():
+        
+        
+
+        opposite_codon_frequencies[key] = (1. - codon_frequencies_dic[key])/64
+    
+
+    return opposite_codon_frequencies
+
+
+
 
 def get_codon_frequencies_doublets(transcriptome):
     '''
@@ -127,7 +145,8 @@ def translate(dna_seq):
         protein_seq = protein_seq + gencode[dna_seq[i*3:(i+1)*3]]
     return protein_seq
     
-def random_reverse_translate(protein_seq, weights_dic = None, change_seq = '', random = True, wiggle = False,
+def random_reverse_translate(protein_seq, weights_dic = None, change_seq = '', random = True, wiggle = False, 
+                                         no_wobble = False,
                                        doubletscode = {}, pairs = True):
     """
     Return a random cDNA sequence for a given protein sequence (protein_seq). 
@@ -144,14 +163,14 @@ def random_reverse_translate(protein_seq, weights_dic = None, change_seq = '', r
             weights = weights_dic[aminoacid]
         if len(change_seq) > 0:
             random_codon = get_random_codon(aminoacid, doubletscode = doubletscode, random = random, wiggle = wiggle,
-                                               weights = weights, prev_codon = prev_codon,
+                                               weights = weights, prev_codon = prev_codon, no_wobble = no_wobble,
                                                    dont_use = change_seq[3*i:3*(i+1)])
             cDNA_seq = cDNA_seq + random_codon
             prev_codon = random_codon
             
         else:
             random_codon = get_random_codon(aminoacid, doubletscode = doubletscode, random = random, wiggle = wiggle,
-                                               weights = weights, prev_codon = prev_codon,
+                                               weights = weights, prev_codon = prev_codon, no_wobble = no_wobble,
                                                    )
             cDNA_seq = cDNA_seq + random_codon
             prev_codon = random_codon
@@ -243,6 +262,25 @@ def get_CAI(dna_seq, weights_dic):
         cai = cai*weights_dic[codon]
     return cai**(1/ncodons)
 
+def get_doublest_likelihood(dna_seq, weights_dic):
+    '''
+    Obtains Codon Adaptation Index (CAI) for a given DNA_seq calculated using weights_dic
+    CAI = (w_1*.w_i*..w_N)^(1/N) where w_i is the weight of codon i. 
+    
+    Inputs:
+        dna_seq: ORF in form of string to evaluate CAI
+        weights_dic: dictionary of CAI weights for each codon. Values are weights and keys are codons. 
+    '''
+    if len(dna_seq) % 3 > 0.:
+        raise ValueError("Length of DNA sequence must be divisble by 3")
+    ncodons = int(len(dna_seq)//3)
+    score = 0. 
+    for i in range(ncodons-1):
+        start = i*3
+        end = start + 6
+        codonpair = dna_seq[start:end].upper()
+        score = score + np.log(weights_dic[codonpair])
+    return score
 
 def get_hamming_dist(seq1, seq2, normalize = True):
     '''
@@ -274,8 +312,10 @@ def get_rnai_score(dna_seq1, dna_seq2, weights_dic, weight_distance = .5):
     return cai, dist, combined_score 
 
 
+
+
 def get_random_codon(aminoacid, aminoacidcode = aminoacidcode, doubletscode = {}, random = True, wiggle = False,
-                     dont_use = '', weights = np.zeros(1),
+                     dont_use = '', weights = np.zeros(1), no_wobble = False,
                     prev_codon = ''):
     '''Gets a random codon for aminoacid, aminoacid must be single letter amino acid code.
     '''
@@ -292,45 +332,31 @@ def get_random_codon(aminoacid, aminoacidcode = aminoacidcode, doubletscode = {}
         
     if dont_use != '':
         ind = codon_list.index(dont_use.upper())
-        
         if len(codon_list) == 2 and wiggle and prev_codon == '':
             extra_weight = .25
-            weights[ind] = weights[ind]*extra_weight
-            
-        else:
-            if ind + 1 == len(codon_list):
-                codon_list = codon_list[:ind]
-                weights = np.array(list(weights[:ind]))
-            else:
-                codon_list = codon_list[:ind] + codon_list[ind + 1:]
-                weights = np.array(list(weights[:ind])  + list(weights[ind + 1:]))  
-    if prev_codon != '':
-        doublets = []
-        weights = []
-        for codon in codon_list:
-            doublets.append(prev_codon + codon)
-            #print(doublets)
-            weights.append(doubletscode[prev_codon + codon])
-        if (len(codon_list) == 1) and dont_use != '':
-            if wiggle:
-                extra_weight = .25
-            else:
-                extra_weight = 0.
-            codon_list.append(dont_use)
-            doublets.append(prev_codon + dont_use)
-            weights.append(doubletscode[prev_codon + dont_use]*extra_weight) # sometimes alternative is terrible, so this allows for more wiggleroom
-          #  if wiggle:
-              #  print(codon_list)
-           
-       # print(codon_list, doublets, weights)
+        else: 
+            extra_weight = 0.0
+        weights[ind] = weights[ind]*extra_weight
+        if no_wobble and len(codon_list) > 2:
+            orig = dont_use[-1]
+            for i in range(len(codon_list)):
+                if i == ind:
+                    continue
+                c = codon_list[i]
+                if (orig == 'C' and c[-1] == 'T') or (orig == 'A' and c[-1] == 'G'):
+                    weights[i] = weights[i]*extra_weight
+
     if random:
+       # print(codon_list, fweights)
         return np.random.choice(codon_list, p = np.array(weights)/np.sum(weights))
     else:
+
         best_ind = np.argmax(weights)
         return codon_list[best_ind]
 
 
 def get_RNAi_seq(ORF, protein, aminoacidweights, gencode_weights, trials = 1000, doubletscode = {}, pairs = True, random = True,
+                 no_wobble = False,
                  enforce_different_codons = False, wiggle = False):
     '''
     Obtains random samples of recoded ORFs that encodes for a protein sequence (protein) based on CAI weights. Evaluates CAI and hamming distance to original ORF. 
@@ -359,16 +385,17 @@ def get_RNAi_seq(ORF, protein, aminoacidweights, gencode_weights, trials = 1000,
         pairs = False
     for i in range(trials):
         if enforce_different_codons:
-            random_seq = random_reverse_translate(protein, random = random,
+            random_seq = random_reverse_translate(protein, random = random, no_wobble = no_wobble,
                                           weights_dic = aminoacidweights, pairs = pairs, doubletscode = doubletscode, wiggle = wiggle,
                                              change_seq = ORF)
         else:
-            random_seq = random_reverse_translate(protein, random = random, pairs = pairs, doubletscode = doubletscode,
+            random_seq = random_reverse_translate(protein, random = random, pairs = pairs, no_wobble = no_wobble, doubletscode = doubletscode,
                                                   wiggle = wiggle,
                                           weights_dic = aminoacidweights)
         seqs.append(random_seq)
         cai, dist, score = get_rnai_score(random_seq, ORF, 
                            gencode_weights, weight_distance = .5)
+      
         scores.append(score)
         cais.append(cai)
         dists.append(dist)
@@ -407,3 +434,91 @@ def read_many_fasta(filename):
                 current_seq = current_seq + line
             
     return descriptors, seqs
+
+
+
+def sliding_window_RNAi_recoding(recode, ORF, protein, aminoacidweights, gencodeweights, 
+                               random = False, no_wobble = False, enforce_different_codons = True, wiggle = False):
+    assert(recode % 3 == 0.)
+        
+    n_recodes = (len(ORF) - recode)//3
+    recode = recode // 3
+    cais = np.zeros(n_recodes)
+    dists = np.zeros(n_recodes)
+    cais_full = np.zeros(n_recodes)
+    dists_full = np.zeros(n_recodes)
+    seqs = []
+    seqs_small = []
+    for i in range(n_recodes):
+        ORF_small = ORF[i*3:3*(i + recode)]
+       # print(len(ORF_small))
+        protein_small = protein[i:i + recode]
+        seq, _, cai, dist = get_RNAi_seq(ORF_small, protein_small, aminoacidweights, gencodeweights, trials = 1, 
+                 random = random,
+                 no_wobble = no_wobble,
+                 enforce_different_codons = enforce_different_codons, wiggle = wiggle)
+        
+        
+        ORF_recode = ORF[:i*3] + seq[0] + ORF[3*(i + recode):]
+        seqs_small.append(seq)
+        #print(len(ORF_recode))
+        cai_full = get_CAI(ORF_recode, gencodeweights)
+        dist_full = get_hamming_dist(ORF_recode, ORF)
+        
+        cais_full[i] = cai_full
+        dists_full[i] = dist_full
+        seqs.append(seq[0])
+        cais[i] = cai[0]
+        dists[i] = dist[0]
+    return seqs, seqs_small, cais, dists, cais_full, dists_full
+
+def adjust_doublet(aminoacidpair, doubletscode, aminoacidcode = aminoacidcode, random = True, wiggle = False,
+                     dont_use_pair= ''*6, weights = np.zeros(1),
+                    prev_codon = ''):
+    '''Gets a random codon for aminoacid, aminoacid must be single letter amino acid code.
+    '''
+  #  print(aminoacidpair)
+    
+    codon_list_start = aminoacidcode[aminoacidpair[0]]
+    codon_list_end = aminoacidcode[aminoacidpair[1]]
+    doublets_cands = []
+    weights_cands = []
+    for codon1 in codon_list_start:
+        if codon1 == dont_use_pair[:3]:
+            continue
+        for codon2 in codon_list_end:
+            if codon2 == dont_use_pair[3:]:
+                continue
+            doublets_cands.append(codon1 + codon2)
+            weights_cands.append(doubletscode[codon1+codon2])
+    if len(weights_cands) == 0:
+        return dont_use_pair
+    best_ind = np.argmax(np.array(weights_cands))
+    if random:
+        return np.random.choice(doublets_cands, p = np.array(weights_cands)/np.sum(weights_cands))
+    return doublets_cands[best_ind]
+    
+
+def adjust(ORF, protein, doubletscode, diff_dic_norm, ORF_no = '', worst = -.5, random = False):
+    codons = len(protein)
+    ORF_new = ORF
+    for i in range(codons - 1):
+        start = i*3
+        end = i*3 + 6
+        if diff_dic_norm[ORF[start:end]] < worst:
+            aminoacidpair = protein[i:i+2]
+            dont_use_pair = ORF_no[start:end]
+            if len(ORF_no) > 0:
+                new_doublet = adjust_doublet(aminoacidpair, doubletscode, dont_use_pair = ORF_no[start:end], random = random)
+            else:
+                new_doublet = adjust_doublet(aminoacidpair, doubletscode, random = random)
+            ORF_new = ORF_new[:start] + new_doublet + ORF_new[end:]
+    assert(translate(ORF_new) == protein)
+    return ORF_new
+            
+    
+def ecdf_vals(data):
+    """Return x and y values for an ECDF."""
+    return np.sort(data), np.arange(1, len(data)+1) / len(data)
+
+
